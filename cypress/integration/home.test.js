6e26a47b4aa1d4052f27013652f6b4d1f7acd7fd
Adam Victor <avictor@printsites.com>
2020-02-10 12:15:57 -0500

    Cybress (end to end) setup | Wrote home page E2E test.

---
+++ cypress/integration/home.test.js
@@ -0,0 +1,32 @@
+
+
+describe('Home Page', () => {
+
+    beforeEach(() => {
+        cy.fixture('courses.json').as("coursesJSON");
+
+        cy.server();
+
+        cy.route('/api/courses', '@coursesJSON').as('courses');
+    });
+
+
+    it('should display a list of courses', () => {
+        cy.visit('/');
+        cy.contains('All Course');
+        cy.wait('@courses');
+
+        cy.get('mat-card').should('have.length', 9);
+    });
+
+    it('should display the advanced courses', () => {
+
+        cy.get('.mat-tab-label').should('have.length', 2);
+        cy.get('.mat-tab-label').last().click();
+        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+        cy.get('.mat-tab-body-active .mat-card-title').first()
+            .should('contain', "Angular Security Course");
+
+    });
+
+});
